efec05b50263b6b9109537505c2c022b827c53a6
carol <carolanne.aleman@gmail.com>
2020-06-23 14:34:59 -0400

    Chat app complete

---
+++ script.js
@@ -0,0 +1,34 @@
+const socket = io('http://localhost:3020')
+const messageBox = document.getElementById('message-box')
+const messageForm = document.getElementById('send-message')
+const messageInput = document.getElementById('message-input')
+
+const name = prompt('What is your name?')
+appendMessage('Welcome to Chatter Box')
+socket.emit('new-user', name)
+
+socket.on('chat-message', data =>{
+    appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name =>{
+    appendMessage(`${name} connected`)
+})
+
+socket.on('user-disconnected', name =>{
+    appendMessage(`${name} disconnected`)
+})
+
+messageForm.addEventListener('submit', e =>{
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${data.message}`)
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
+})
+
+function appendMessage(message){
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageBox.append(messageElement)
+}
